7dbecd0d243db871b3348ff6c389829761f9936f
jujusinhodoscapa <166841741+jujusinhodoscapa@users.noreply.github.com>
2024-04-12 13:48:44 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Sad)
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . # # # .
+    # . . . #
+    `)
+basic.forever(function () {
+	
+})
